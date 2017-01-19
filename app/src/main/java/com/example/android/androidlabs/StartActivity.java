45b7b19c40d119eb7a4a40eacd809d585100cf03
octagonq <arna0008@algonquinlive.com>
2017-01-19 15:11:10 -0500

    in class changes

--- app/src/main/java/com/example/android/androidlabs/StartActivity.java
+++ app/src/main/java/com/example/android/androidlabs/StartActivity.java
@@ -2,6 +2,9 @@ package com.example.android.androidlabs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -9,5 +12,8 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+
+
     }
 }
