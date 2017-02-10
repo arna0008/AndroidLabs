3d426d0aacca4b330e52aff7412dd7ad233b01f1
octagonq <arna0008@algonquinlive.com>
2017-02-09 23:23:18 -0500

    Committed changes to week 3 lab

--- app/src/main/java/com/example/android/androidlabs/LoginActivity.java
+++ app/src/main/java/com/example/android/androidlabs/LoginActivity.java
@@ -1,29 +1,146 @@
 package com.example.android.androidlabs;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import static android.R.attr.button;
+
 
 public class LoginActivity extends AppCompatActivity {
+    //for tagging
+    protected static final String ACTIVITY_NAME = "LoginActivity";
+
+    //email login
+    EditText emailField; //enter email
+    Button loginButton; //click to login
+
+
+    //for storing saved preference
+    SharedPreferences prefs;
+    SharedPreferences.Editor prefsEditor; //used to write a preference
+
+    //email ID
+    String emailId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        Log.i(ACTIVITY_NAME, "onCreate()");
+
+        /*  LOGIN button, and email field   */
+        loginButton = (Button) findViewById(R.id.loginButton);
+        emailField = (EditText) findViewById(R.id.inputEmail);
+        prefs = PreferenceManager.getDefaultSharedPreferences(LoginActivity.this); //gets default shared prefs for Login Activity class
+
+
+        loginButtonClick(emailField);
+
+    }//end onCreate
+
+
+
+    protected void loginButtonClick(View view)
+    {
+        Log.i("Message", "Login Button Clicked");
+
+       /* prefsEditor = prefs.edit(); //use editor to write to prefs object
+        prefsEditor.putString("email", emailField.getText().toString());
+        prefsEditor.commit(); //commit changes to prefs
+        */
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                //Get an editor object for writing to the file:
+                SharedPreferences.Editor prefsEditor = prefs.edit();
+
+                prefsEditor.putString("email", emailField.getText().toString());
+
+                //Write the file:
+                prefsEditor.commit();
+
+                Intent intent = new Intent(LoginActivity.this, StartActivity.class);
+                startActivity(intent);
+
             }
         });
     }
 
+
+
+
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "In onResume()");
+
+        if(prefs != null && emailId != null){
+            emailId = prefs.getString("email", "No Email");
+        }else{
+            emailId = prefs.getString("DefaultEmail", "email@domain.com");
+        }
+
+        //set the text for the email field
+        emailField.setText(emailId);
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(ACTIVITY_NAME, "In onStart()");
+
+        if(prefs != null && emailId != null){
+            emailId = prefs.getString("DefaultEmail", "No Email");
+        }else{
+            emailId = prefs.getString("DefaultEmail", "email@domain.com");
+        }
+
+        //set the text for the email field
+        emailField.setText(emailId);
+    }
+
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "In onPause()");
+
+
+        prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        prefsEditor = prefs.edit();
+        prefsEditor.putString("DefaultEmail",emailField.getText().toString());
+        prefsEditor.commit();
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(ACTIVITY_NAME, "In onStop()");
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, "In onDestroy()");
+    }
 }
--- app/src/main/java/com/example/android/androidlabs/StartActivity.java
+++ app/src/main/java/com/example/android/androidlabs/StartActivity.java
@@ -1,19 +1,101 @@
 package com.example.android.androidlabs;
 
+import android.app.Activity;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class StartActivity extends AppCompatActivity {
+
+    //customized tagging
+    protected static final String ACTIVITY_NAME = "StartActivity";
+
+    //Instantiate Button to get to ListItemActivity
+    Button goToListActivityButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
+        //a log
+        Log.i(ACTIVITY_NAME,"In onCreate");
+
+        //set button to id correct id
+        goToListActivityButton = (Button) findViewById(R.id.goToLoginActivityButtonId);
+
+
+        //Click button to go to ListItemsActivity
+        goToListActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Intent to go from StartActivity(this) to ListItemsActivity
+                Intent intentToListItems = new Intent(StartActivity.this, ListItemsActivity.class);
+                startActivityForResult(intentToListItems, 5);
+
+
+            }
+        });
 
 
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 5 && resultCode == Activity.RESULT_OK) {
+
+            Log.i(ACTIVITY_NAME, "Returned to StartActivity.onActivityResult");
+
+            //get String passed from ListItemsActivity
+            String messagePassed = data.getStringExtra("Response");
+
+            Toast.makeText(this, messagePassed, Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+        @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(ACTIVITY_NAME, "In onStart");
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "In onResume");
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "In onPause");
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(ACTIVITY_NAME, "In onStop");
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, "In onDestroy");
+    }
+
+
+
 }
